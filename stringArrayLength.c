913dbb15c518d93b62019f037a5c20498f12e8d0
Manik <tonysins43@gmail.com>
2024-10-22 22:33:02 +0600

    find string length with library funciton

--- stringArrayLength.c
+++ stringArrayLength.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
-
+#include<string.h>
 int main(){
-    char str[10] = "abcd";
+    char str[10] = "abhrtjr";
+    
+    // by using normal loop---------
+    // int length = 0;
+    // for(int i = 0;str[i] != '\0';i++){
+    //     length++;
+    // }
 
-    int length = 0;
-    for(int i = 0;str[i] != '\0';i++){
-        length++;
-    }
+    // by using library function------------
+    int length = strlen(str);
 
     printf("%d\n",length);
 
